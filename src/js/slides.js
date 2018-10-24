3bf80e045c78076d43e9c60bc840035fd9a09be9
“Kevin <kevin.five@kunstmaan.be>
2018-10-24 15:04:48 +0200

    u

--- src/js/slides.js
+++ src/js/slides.js
@@ -1,6 +1,9 @@
+import 'reveal.js/lib/js/head.min';
 import Reveal from 'reveal.js';
+import hljs from 'highlight.js';
 
 export default function slides() {
     Reveal.initialize();
+    hljs.initHighlightingOnLoad();
 }
 
